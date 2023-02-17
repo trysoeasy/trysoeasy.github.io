684f8deb272bcf7746741e245b851211e9d18c50
DESKTOP-JNVG1PO\lq <lq65210@163.com>
2023-02-17 14:36:40 +0800

    11111

--- next.config.js
+++ next.config.js
@@ -1,21 +1,20 @@
-// next.config.js
-const withNextra = require("nextra")({
-  // 主题包名
-  theme: "nextra-theme-docs",
-  // 主题配置文件
-  themeConfig: "./theme.config.tsx",
-  // 给文档中的代码块添加 copy 能力
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
   defaultShowCopyCode: true,
-  // 支持 latex
   latex: true,
-  // 支持静态图片
   staticImage: true,
-});
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // next 工程本身配置内容
   reactStrictMode: true,
-};
+  images: {
+    unoptimized: true
+  }
+}
 
-module.exports = withNextra(nextConfig);
+module.exports = withNextra(nextConfig)
+ 
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
